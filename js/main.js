df6ee8f724a64844ac22573d641c2c5b09125435
Karli <karla.anhdz@gmail.com>
2022-12-14 13:06:27 -0600

    Creación de Tablas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 // aquí va el código
+//btnAgregar 
+//alertValidacionesTexto
+//alertVAlidadciones
+//contadorProductos
+//productosTotal
+//precioTotal
+
+let txtNombre = document.getElementById("Name");
+let txtNumber = document.getElementById("Number");
+let total = document.getElementById("precioTotal");
+
+let tabla = document.getElementById("tablaListaCompras");
+let cuerpoTabla = document.getElementById("tbody");
+
+let btnAgregar = document.getElementById("btnAgregar");
+
+btnAgregar.addEventListener("click", function(event){
+    event.preventDefault
+}
